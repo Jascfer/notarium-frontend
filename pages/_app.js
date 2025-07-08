2406182645ffd02727f763191eacefe6c7a71724
Jascfer <ozgurxspeaktr@gmail.com>
2025-07-08 06:00:53 +0300

    remove: vercel analytics for build compatibility

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import { AuthProvider } from '../contexts/AuthContext'
 import { ThemeProvider } from '../contexts/ThemeContext'
 import Navbar from '../components/Navbar'
-import { Analytics } from '@vercel/analytics/react';
+// import { Analytics } from '@vercel/analytics/react'; // KALDIRILDI
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
         <div className="min-h-screen">
           <Navbar />
           <Component {...pageProps} />
-          <Analytics />
+          {/* <Analytics /> */} {/* KALDIRILDI */}
         </div>
       </ThemeProvider>
     </AuthProvider>
