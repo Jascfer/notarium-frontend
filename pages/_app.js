24b079102b600a511648ea0f073d8d1a81c1e35f
Jascfer <ozgurxspeaktr@gmail.com>
2025-07-14 03:26:28 +0300

    ii

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import { AuthProvider } from '../contexts/AuthContext'
 import { ThemeProvider } from '../contexts/ThemeContext'
 import Navbar from '../components/Navbar'
-import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,7 +10,6 @@ export default function App({ Component, pageProps }) {
         <div className="min-h-screen">
           <Navbar />
           <Component {...pageProps} />
-          <Analytics />
         </div>
       </ThemeProvider>
     </AuthProvider>
